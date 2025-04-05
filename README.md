a210190f85fae233bdfaccf7536d2c202d3add52
Sekoucisse.ehb <cissesekou2000@hotmail.com>
2025-04-06 00:35:59 +0200

    modifie de file for analyse.py

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # script_python_cybersecurite
- Script_python_cybersecurite learning 
+ Script_python_cybersecurite learning
+ 
--- script/analyse.py
+++ script/analyse.py
@@ -1,12 +1,12 @@
 import argparse
 
-def main():
-    parse  =argparse.ArgumentParser(description= "Script d'analyse")
-    parse .add_argument("logpath", help="Chemin vers les fichiers de logs à analyser", type=str)
-    args = parse.parse_args()
+
+def main(): 
+    parser = argparse.ArgumentParser(description= "Script d'analyse")
+    parser.add_argument("logpath", help="Chemin vers les fichiers de logs à analyser", type=str)
+    args = parser.parse_args()
     
-    print ( f"Chemin des logs à analyser : { args.logpath}")
+    print(f"Chemin des logs à analyser : { args.logpath}")
 
     if __name__ == "__main__":
-    
         main()
